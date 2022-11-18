300f73bcb13cb3c8d797da3306d9f5ea56e2836b
juanjuan07 <92894505+juanjuan07@users.noreply.github.com>
2022-11-17 19:41:32 -0600

    ensayos

---
+++ ensayo3.js
@@ -0,0 +1,48 @@
+function sumar(sum1, sum2)
+{ 
+    var resultado;
+     resultado = sum1 + sum2;
+      return resultado;
+} 
+console.log("resultado de la suma es: ",sumar(245,744));
+
+function restar(sum3, sum4)
+{ 
+    var resultado2;
+     resultado2 = sum3 - sum4;
+      return resultado2;
+} 
+console.log("resultado de la resta es: ",restar(245,744));
+
+function multipicar(sum5, sum6)
+{ 
+    var resultado3;
+     resultado3 = sum5 * sum6;
+      return resultado3;
+} 
+console.log("resultado de la multipicacion es: ",multipicar(2,44));
+
+
+function division(sum7, sum8)
+{ 
+    var resultado4;
+     resultado4 = sum7 / sum8;
+      return resultado4;
+} 
+console.log("resultado de la division es: ",division(4,2));
+
+function cuadrado(sum9)
+{ 
+    var resultado5;
+     resultado5 = sum9 * sum9;
+      return resultado5;
+} 
+console.log("resultado del cuadrado de un numero es: ", cuadrado(7));
+
+function cubo(sum10)
+{ 
+    var resultado6;
+     resultado6 = sum10 * sum10;
+      return resultado6;
+} 
+console.log("resultado al cubo de un numero es: ", cuadrado(8));
---
+++ esayo2.js
@@ -0,0 +1,5 @@
+var num1 = 10; 
+var num2 = 50; 
+9 * num1;
+ num2 / num1;                     
+console.log();
